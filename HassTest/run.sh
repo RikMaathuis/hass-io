822cc67ba53b848553899d4ddfc3eba9cff29426
vingerindebips <rik.maathuis@gmail.com>
2021-03-08 15:48:24 +0100

    YEP

--- HassTest/run.sh
+++ HassTest/run.sh
@@ -1,15 +1,3 @@
-#!/bin/bash
-set -e
+#!/usr/bin/with-contenv bashio
 
-echo "starting!"
-
-# Read from STDIN aliases to play  file
-while read -r input; do
-    # removing JSON stuff
-    input="$(echo "$input" | jq --raw-output '.')"
-    echo "[Info] Read alias: $input"
-
-    if ! msg="$(play $input)"; then
-    	echo "[Error] Playing failed -> $msg"
-    fi
-done
+echo Hello world!
