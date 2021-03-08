9e339198646f98b0301600a12f4ef8c9f8ea3ed3
vingerindebips <rik.maathuis@gmail.com>
2021-03-09 00:04:13 +0100

    j

--- HassTest/run.sh
+++ HassTest/run.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
-set -e
-
+#!/usr/bin/with-contenv bashio
+declare -a options
+bashio::log.info 'Starting the Music daemon...'
 echo "starting!"
-
+/bin/sh -c "chmod u+s /dev/vchiq"
+bashio::log.info 'fuckk'
+#else msg='/bin/sh -c "chmod u+s /dev/vchiq"';
 # Read from STDIN aliases to play  file
 while read -r input; do
     # removing JSON stuff
     input="$(echo "$input" | jq --raw-output '.')"
+	
     echo "[Info] Read alias: $input"
-
-    if ! msg="$(play $input)"; then
+	
+    if ! msg="$(omxplayer -o local $input)"; then
     	echo "[Error] Playing failed -> $msg"
     fi
 done
